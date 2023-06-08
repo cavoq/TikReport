9c5a33d9b78ee63e0e0d8fa015e64f1eced71264
David Stromberger <DavidStromberger99@gmx.de>
2023-06-08 16:29:38 +0200

    update isValidProxy, getBrowser

--- tik-report.ts
+++ tik-report.ts
@@ -1,4 +1,6 @@
 import fs from "fs";
+import puppeteer, { Browser } from "puppeteer";
+
 
 const CONFIG_FILE = "config.json";
 
@@ -9,10 +11,47 @@ function readJsonFile(filename: string): { [key: string]: any } {
   return data;
 }
 
-function readProxyList(file: string) {
+function readProxyList(file: string): string[] {
   const list = fs.readFileSync(file, "utf8");
-  return list.split("\n");
+  const proxyList = list.split("\n");
+  const validProxies: string[] = [];
+
+  for (const proxy of proxyList) {
+    if (isValidProxy(proxy)) {
+      validProxies.push(proxy);
+    }
+  }
+
+  return validProxies;
 }
+
+function isValidProxy(proxy: string): boolean {
+  const [host, port] = proxy.split(":");
+  if (!host || !port) {
+    return false;
+  }
+  if (isNaN(Number(port))) {
+    return false;
+  }
+  // Validate IPv4 address
+  const ipv4Pattern = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+  if (!ipv4Pattern.test(host)) {
+    return false;
+  }
+
+  return true;
+}
+
+async function getBrowser(proxy: string): Promise<Browser> {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: [
+      `--proxy-server=${proxy}`,
+    ],
+  });
+  return browser;
+}
+
 
 export {
   readJsonFile,
